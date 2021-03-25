089ad0928f9086f3a250feebdb8b250b99dcda4b
edwardlee03 <bert825_work@163.com>
2021-03-26 01:43:22 +0800

    play module and method call

--- hello.go
+++ hello.go
@@ -1,8 +1,12 @@
+// 程序入口点
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-learning/tutorial"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("你好，世界！")
+	fmt.Println(tutorial.HelloEnglish())
+	fmt.Println(tutorial.HelloChinese())
 }
---
+++ tutorial/tutorial.go
@@ -0,0 +1,9 @@
+package tutorial
+
+func HelloEnglish() string {
+	return "Hello, World!"
+}
+
+func HelloChinese() string {
+	return "你好，世界！"
+}
