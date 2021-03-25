4bb5d0b86afbee483f427fbde5208f696f80380f
edwardlee03 <bert825_work@163.com>
2021-03-26 02:04:14 +0800

    variables are explicitly declared and used by the compiler to check type-correctness of function calls

---
+++ example/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var m, n int = 1, 2
+	fmt.Println(m, n)
+
+	var b = true
+	fmt.Println(b)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
