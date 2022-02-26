f3723ed2cd7487b1a0e5bbbc7c44ed34d3fa313b
edwardlee03 <bert825_work@163.com>
2022-02-26 16:40:26 +0800

    improve example Constants

--- example/constants.go
+++ example/constants.go
@@ -5,14 +5,28 @@ import (
 	"math"
 )
 
+// 声明一个常量值
 const s string = "constant"
 
-// Go supports constants of character, string, boolean, and numeric values.
+/*
+Go supports constants of character, string, boolean, and numeric values.
+
+const declares a constant value.
+
+A const statement can appear anywhere a var statement can.
+
+Constant expressions perform arithmetic with arbitrary precision.
+
+A numeric constant has no type until it’s given one, such as by an explicit conversion.
+
+A number can be given a type by using it in a context that requires one, such as a variable assignment or function call.
+For example, here math.Sin expects a float64.
+*/
 func main() {
 	fmt.Println(s)
 	// constant
 
-	const n = 5_0000_0000
+	const n = 500_000_000
 
 	const d = 3e20 / n
 	fmt.Println(d)
