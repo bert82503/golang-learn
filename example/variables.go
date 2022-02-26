e958ab438a56497acfa226cab5c3006e68532753
edwardlee03 <bert825_work@163.com>
2022-02-26 16:33:41 +0800

    improve example Variables

--- example/variables.go
+++ example/variables.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
-// Variables are explicitly declared and used by the compiler to check type-correctness of function calls.
+/*
+In Go, variables are explicitly declared and used by the compiler to e.g. check type-correctness of function calls.
+
+var declares 1 or more variables.
+
+You can declare multiple variables at once.
+
+Go will infer the type of initialized variables.
+
+Variables declared without a corresponding initialization are zero-valued.
+For example, the zero value for an int is 0.
+
+The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "apple" in this case.
+*/
 func main() {
 	var a = "initial"
 	fmt.Println(a)
