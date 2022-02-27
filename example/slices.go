d492ebd88eeb90f56338045de164675695abaae1
edwardlee03 <bert825_work@163.com>
2022-02-27 23:55:34 +0800

    improve example Slices

--- example/slices.go
+++ example/slices.go
@@ -2,9 +2,45 @@ package main
 
 import "fmt"
 
-// Slices are a key data type, giving a more powerful interface to sequences than arrays.
-// more details on the design and implementation of slices in Go.
-// Go slices usage and internals, https://blog.golang.org/2011/01/go-slices-usage-and-internals.html
+/*
+Slices are a key data type in Go, giving a more powerful interface to sequences than arrays.
+(类似Java的StringBuilder)
+
+Unlike arrays, slices are typed only by the elements they contain (not the number of elements).
+To create an empty slice with non-zero length, use the builtin make.
+Here we make a slice of strings of length 3 (initially zero-valued).
+
+We can set and get just like with arrays.
+
+len returns the length of the slice as expected.
+
+In addition to these basic operations, slices support several more that make them richer than arrays.
+One is the builtin append, which returns a slice containing one or more new values.
+Note that we need to accept a return value from append as we may get a new slice value.
+
+Slices can also be copy’d.
+Here we create an empty slice c of the same length as s and copy into c from s.
+
+Slices support a “slice” operator with the syntax slice[low:high].
+For example, this gets a slice of the elements s[2], s[3], and s[4].
+
+This slices up to (but excluding) s[5].
+
+And this slices up from (and including) s[2].
+
+We can declare and initialize a variable for slice in a single line as well.
+
+Slices can be composed into multi-dimensional data structures.
+The length of the inner slices can vary, unlike with multi-dimensional arrays.
+
+Note that while slices are different types than arrays, they are rendered similarly by fmt.Println.
+
+Check out this great blog post by the Go team for more details on the design and implementation of slices in Go.
+more details on the design and implementation of slices in Go.
+(Go slices usage and internals, https://blog.golang.org/2011/01/go-slices-usage-and-internals.html)
+
+Now that we’ve seen arrays and slices we’ll look at Go’s other key builtin data structure: maps.
+*/
 func main() {
 	s := make([]string, 3)
 	fmt.Println("emp:", s)
