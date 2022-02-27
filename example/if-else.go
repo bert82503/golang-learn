d6f2b6ab9ae75996abab0d68a31368e8c3d6e73a
edwardlee03 <bert825_work@163.com>
2022-02-27 23:31:41 +0800

    improve example If/Else

--- example/if-else.go
+++ example/if-else.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
-// Branching with if and else in Go is straight-forward.
+/*
+Branching with if and else in Go is straight-forward.
+
+Here’s a basic example.
+
+You can have an if statement without an else.
+
+A statement can precede conditionals; any variables declared in this statement are available in all branches.
+
+Note that you don’t need parentheses around conditions in Go, but that the braces are required.
+
+There is no ternary if in Go, so you’ll need to use a full if statement even for basic conditions.
+*/
 func main() {
 	if 7%2 == 0 {
 		fmt.Println("7 is even")
--- example/variables.go
+++ example/variables.go
@@ -14,7 +14,8 @@ Go will infer the type of initialized variables.
 Variables declared without a corresponding initialization are zero-valued.
 For example, the zero value for an int is 0.
 
-The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "apple" in this case.
+The := syntax is shorthand for declaring and initializing a variable,
+e.g. for var f string = "apple" in this case.
 */
 func main() {
 	var a = "initial"
