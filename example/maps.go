af92bf2af4f39aaf565e872a153a8c78fdc12449
edwardlee03 <bert825_work@163.com>
2022-02-28 00:00:01 +0800

    improve example Maps

--- example/maps.go
+++ example/maps.go
@@ -4,6 +4,30 @@ import "fmt"
 
 // Maps are Go’s built-in associative data type
 // (sometimes called hashes or dicts in other languages).
+/*
+Maps are Go’s built-in associative data type (sometimes called hashes or dicts in other languages).
+(关联数据类型，哈希表/散列表或字典)
+
+To create an empty map, use the builtin make: make(map[key-type]val-type).
+
+Set key/value pairs using typical name[key] = val syntax.
+
+Printing a map with e.g. fmt.Println will show all of its key/value pairs.
+
+Get a value for a key with name[key].
+
+The builtin len returns the number of key/value pairs when called on a map.
+
+The builtin delete removes key/value pairs from a map.
+
+The optional second return value when getting a value from a map indicates if the key was present in the map.
+This can be used to disambiguate between missing keys and keys with zero values like 0 or "".
+Here we didn’t need the value itself, so we ignored it with the blank identifier _.
+
+You can also declare and initialize a new map in the same line with this syntax.
+
+Note that maps appear in the form map[k:v k:v] when printed with fmt.Println.
+*/
 func main() {
 	// create an empty map, use the builtin make: make(map[key-type]val-type).
 	m := make(map[string]int)
