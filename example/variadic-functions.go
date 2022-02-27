4824846870e0562b4a346969f1d08c37a1f740d3
edwardlee03 <bert825_work@163.com>
2022-02-28 00:12:52 +0800

    improve example Variadic Functions

--- example/variadic-functions.go
+++ example/variadic-functions.go
@@ -12,7 +12,19 @@ func sum(nums ...int) {
 	fmt.Println(total)
 }
 
-// Variadic functions can be called with any number of trailing arguments.
+/*
+Variadic functions can be called with any number of trailing arguments.
+For example, fmt.Println is a common variadic function.
+
+Here’s a function that will take an arbitrary number of ints as arguments.
+
+Variadic functions can be called in the usual way with individual arguments.
+
+If you already have multiple args in a slice, apply them to a variadic function using func(slice...) like this.
+
+Another key aspect of functions in Go is their ability to form closures,
+which we’ll look at next.
+*/
 func main() {
 	sum(1, 2)
 	// [1 2] 3
