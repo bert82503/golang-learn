eb6363962c0d65c3584ace30d4bd2c2ba54b99d8
edwardlee03 <bert825_work@163.com>
2022-02-28 00:06:43 +0800

    improve example Functions

--- example/functions.go
+++ example/functions.go
@@ -13,7 +13,22 @@ func plusPlus(a, b, c int) int {
 	return a + b + c
 }
 
-// Functions are central in Go.
+/*
+Functions are central in Go.
+We’ll learn about functions with a few different examples.
+
+Here’s a function that takes two ints and returns their sum as an int.
+
+Go requires explicit returns, i.e. it won’t automatically return the value of the last expression.
+
+When you have multiple consecutive parameters of the same type,
+you may omit the type name for the like-typed parameters up to the final parameter that declares the type.
+
+Call a function just as you’d expect, with name(args)
+
+There are several other features to Go functions.
+One is multiple return values, which we’ll look at next.
+*/
 func main() {
 	res := plus(1, 2)
 	fmt.Println("1+2 =", res)
