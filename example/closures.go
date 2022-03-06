4c03e3dc5f0b69466825c0fc70a9f7d408fd2d40
edwardlee03 <bert825_work@163.com>
2022-03-06 12:13:09 +0800

    improve example Closures

--- example/closures.go
+++ example/closures.go
@@ -11,8 +11,22 @@ func intSeq() func() int {
 	}
 }
 
-// Go supports anonymous functions, which can form closures.
-// Anonymous functions are useful when you want to define a function inline without having to name it.
+/*
+Go supports anonymous functions, which can form closures.
+Anonymous functions are useful when you want to define a function inline without having to name it.
+
+This function intSeq returns another function, which we define anonymously in the body of intSeq.
+The returned function closes over the variable i to form a closure.
+
+We call intSeq, assigning the result (a function) to nextInt.
+This function value captures its own i value, which will be updated each time we call nextInt.
+
+See the effect of the closure by calling nextInt a few times.
+
+To confirm that the state is unique to that particular function, create and test a new one.
+
+The last feature of functions we’ll look at for now is recursion.
+*/
 func main() {
 	nextInt := intSeq()
 
