16b1854168b654c9a3c433b43fbf95fd37b7e051
edwardlee03 <bert825_work@163.com>
2022-03-06 18:33:44 +0800

    improve example Recursion

--- example/closures.go
+++ example/closures.go
@@ -14,6 +14,7 @@ func intSeq() func() int {
 /*
 Go supports anonymous functions, which can form closures.
 Anonymous functions are useful when you want to define a function inline without having to name it.
+(匿名函数，闭包)
 
 This function intSeq returns another function, which we define anonymously in the body of intSeq.
 The returned function closes over the variable i to form a closure.
--- example/recursion.go
+++ example/recursion.go
@@ -9,12 +9,34 @@ func fact(n int) int {
 	if n == 0 {
 		return 1
 	}
-	// 递归调用，分而治之思想
+	// 递归调用，分而治之的思想
 	return n * fact(n-1)
 }
 
-// Go supports recursive functions.
+/*
+Go supports recursive functions.
+Here’s a classic example.
+(递归函数)
+
+This fact function calls itself until it reaches the base case of fact(0).
+(终止条件)
+
+Closures can also be recursive, but this requires the closure to be declared with a typed var explicitly before it’s defined.
+
+Since fib was previously declared in main, Go knows which function to call with fib here.
+*/
 func main() {
 	fmt.Println(fact(7))
 	// 5040
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
