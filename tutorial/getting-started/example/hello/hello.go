adf5ad200e32cc8c98485c683176b50c7896a238
lihuagang <lihuagang@shinemo.com>
2022-11-08 20:40:24 +0800

    [Tutorial: Get started with Go] 5. Paste the following code into your hello.go file and save the file.

---
+++ tutorial/getting-started/example/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Tutorial: Get started with Go
+// https://golang.google.cn/doc/tutorial/getting-started
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("你好，世界！")
+}
