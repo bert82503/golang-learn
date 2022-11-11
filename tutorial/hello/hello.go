2bfba564b280ba32469171069634622684966de9
bert82503 <bert825_work@163.com>
2022-11-11 16:57:33 +0800

    improve "Call code in an external package" of <Tutorial: Get started with Go>

--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"rsc.io/quote"
+)
 
 // Getting started - Say Hello, World with Go.
 // Tutorial: Get started with Go
 // https://golang.google.cn/doc/tutorial/getting-started
-// Write some code
 
 func main() {
+	// Write some code
 	fmt.Println("Hello, World!")
 	fmt.Println("你好，世界！")
+
+	// Call code in an external package
+	fmt.Println(quote.Go())
 }
