18f9abccfe73a47349627b29def757ecb044e0a2
bert82503 <bert825_work@163.com>
2022-11-21 14:33:58 +0800

    add "Go 离线版教程" of <Go 指南>

---
+++ tour/1.welcome/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func hello() {
+	fmt.Println("Hello, 世界")
+	fmt.Println("开始学习Go语言指南！")
+}
---
+++ tour/1.welcome/sandbox.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sandbox() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
