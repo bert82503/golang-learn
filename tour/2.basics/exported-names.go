63d733f6366a399efb04d868025647414680d82c
bert82503 <bert825_work@163.com>
2022-11-21 19:51:08 +0800

    improve "Exported names 导出名" of "Basics 基础" in <Go 指南>

--- tour/2.basics/exported-names.go
+++ tour/2.basics/exported-names.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
 func main() {
-  // cannot refer to unexported name math.pi
-  // undefined: math.pi
-  //fmt.Println(math.pi)
+	// # command-line-arguments
+	// .\exported-names.go:11:19: undefined: math.pi
+	//fmt.Println(math.pi)
 
-  fmt.Println(math.Pi)
+	fmt.Println(math.Pi)
 }
