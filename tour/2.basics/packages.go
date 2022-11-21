41a1075440e28872c3516e4d87f48b527d397ef6
bert82503 <bert825_work@163.com>
2022-11-21 19:28:34 +0800

    improve "Packages 包" of "Basics 基础" in <Go 指南>

--- tour/2.basics/packages.go
+++ tour/2.basics/packages.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// To see a different number, seed the number generator; see `rand.Seed`.
-	rand.Seed(time.Now().UnixMilli())
+	rand.Seed(time.Now().UnixNano())
 
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
