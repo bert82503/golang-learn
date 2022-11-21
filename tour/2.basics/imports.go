31858120a4bc499dfce0b879b6b78ea4e81f7a39
bert82503 <bert825_work@163.com>
2022-11-21 19:43:41 +0800

    improve "Imports 导入" of "Basics 基础" in <Go 指南>

--- tour/2.basics/imports.go
+++ tour/2.basics/imports.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
-func main()  {
-  fmt.Printf("Now you have %g problems.", math.Sqrt(9))
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
