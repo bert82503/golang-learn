e1111b59e52a7c6b85d1982c120c68c519eaec9c
bert82503 <bert825_work@163.com>
2022-11-23 19:28:21 +0800

    add "6.If with a short statement if的简短语句" of "Basics 基础" in <Go 指南>

--- tour/3.flowcontrol/if-with-a-short-statement.go
+++ tour/3.flowcontrol/if-with-a-short-statement.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-  "math"
-  "fmt"
+	"fmt"
+	"math"
 )
 
 func pow(x, n, lim float64) float64 {
-  // 代码可读性更好
-  //v := math.Pow(x, n)
-  //if v < lim {
-  if v := math.Pow(x, n); v < lim {
-    return v
-  }
-  return lim
-  // 变量作用域
-  // prog.go:12:9: undefined: v
-  //return v
+	// 代码可读性更好
+	//v := math.Pow(x, n)
+	//if v < lim {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+	// 变量作用域
+	// prog.go:12:9: undefined: v
+	//return v
 }
 
 func main() {
-  fmt.Println(
-    pow(3, 2, 10),
-    pow(3, 3, 20),
-  )
+	// 9 20
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }
