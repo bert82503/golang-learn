6366d4c69fcd1df3b098cace97746c219d961e0f
bert82503 <bert825_work@163.com>
2022-11-23 19:32:12 +0800

    add "7.If and else if和else" of "Basics 基础" in <Go 指南>

--- tour/3.flowcontrol/if-and-else.go
+++ tour/3.flowcontrol/if-and-else.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-  "math"
-  "fmt"
+	"fmt"
+	"math"
 )
 
 func pow2(x, n, lim float64) float64 {
-  if v := math.Pow(x, n); v < lim {
-    return v
-  } else {
-    fmt.Printf("%g >= %g\n", v, lim)
-  }
-  // 这里开始就不能使用 v 了
-  return lim
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	// 这里开始就不能使用 v 了
+	return lim
 }
 
+/*
+27 >= 20
+9 20
+*/
 func main() {
-  fmt.Println(
-    pow2(3, 2, 10),
-    pow2(3, 3, 20),
-  )
+	fmt.Println(
+		pow2(3, 2, 10),
+		pow2(3, 3, 20),
+	)
 }
