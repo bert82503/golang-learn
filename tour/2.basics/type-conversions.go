7a60a39eb65f34413d42cf17c8c30818743678b5
bert82503 <bert825_work@163.com>
2022-11-23 16:42:40 +0800

    add "13.Type conversions 类型转换" of "Basics 基础" in <Go 指南>

--- tour/2.basics/type-conversions.go
+++ tour/2.basics/type-conversions.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "math"
-  "fmt"
+	"fmt"
+	"math"
 )
 
 func main() {
-  var x, y int = 3, 4
-  var f float64 = math.Sqrt(float64(x*x + y*y))
-  var z uint = uint(f)
-
-  fmt.Println(x, y, z)
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	// 3 4 5
+	fmt.Println(x, y, z)
 }
