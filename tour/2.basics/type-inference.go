00cef375fef3802c67262a1c8aa28ac93fd27448
bert82503 <bert825_work@163.com>
2022-11-23 16:54:49 +0800

    add "14.Type inference 类型推导" of "Basics 基础" in <Go 指南>

--- tour/2.basics/type-inference.go
+++ tour/2.basics/type-inference.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  v := 4 // 修改这里！
-  fmt.Printf("v is of type %T\n", v)
+	v := 42 // change me!
+	// v is of type int
+	fmt.Printf("v is of type %T\n", v)
 }
