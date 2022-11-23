6b21eb3389df3065f056a2a3186a3e8a304d703b
bert82503 <bert825_work@163.com>
2022-11-23 15:57:06 +0800

    add "10.Short variable declarations 短变量声明" of "Basics 基础" in <Go 指南>

--- tour/2.basics/short-variable-declarations.go
+++ tour/2.basics/short-variable-declarations.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  var i, j = 1, 2
-  k := 3
-  c, python, java := true, false, "no!"
-  fmt.Println(i, j, k, c, python, java)
+	//var i, j int = 1, 2
+	var i, j = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+	// 1 2 3 true false no!
+	fmt.Println(i, j, k, c, python, java)
 }
