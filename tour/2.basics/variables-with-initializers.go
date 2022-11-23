804ab835b69f30ad1166a63f104ac0cbf11c2088
bert82503 <bert825_work@163.com>
2022-11-23 15:50:37 +0800

    add "9.Variables with initializers 变量的初始化" of "Basics 基础" in <Go 指南>

--- tour/2.basics/variables-with-initializers.go
+++ tour/2.basics/variables-with-initializers.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+// var i, j int = 1, 2
 var i, j = 1, 2
 
 func main() {
-  var c, python, java = true, false, "no!"
-  fmt.Println(i, j, c, python, java)
+	var c, python, java = true, false, "no!"
+	// 1 2 true false no!
+	fmt.Println(i, j, c, python, java)
 }
