255185bb521ed06ed26c0e5477acd1a47c98b679
bert82503 <bert825_work@163.com>
2022-11-23 16:17:19 +0800

    add "12.Zero values 零值" of "Basics 基础" in <Go 指南>

--- tour/2.basics/zero-values.go
+++ tour/2.basics/zero-values.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  var i int
-  var f float64
-  var b bool
-  var s string
-
-  fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	var i int
+	var f float64
+	var b bool
+	var s string
+	// 0 0 false ""
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
