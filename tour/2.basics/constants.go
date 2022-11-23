1f2ca9e98a9755b626d85a7b95cf8c386c71873c
bert82503 <bert825_work@163.com>
2022-11-23 16:58:11 +0800

    add "15.Constants 常量" of "Basics 基础" in <Go 指南>

--- tour/2.basics/constants.go
+++ tour/2.basics/constants.go
@@ -4,11 +4,16 @@ import "fmt"
 
 const Pi = 3.14
 
+/*
+Hello 世界
+Happy 3.14 Day
+Go rules? true
+*/
 func main() {
-  const World = "世界"
-  fmt.Println("Hello", World)
-  fmt.Println("Happy", Pi, "Day")
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
 
-  const Truth = true
-  fmt.Println("Go rules?", Truth)
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
 }
