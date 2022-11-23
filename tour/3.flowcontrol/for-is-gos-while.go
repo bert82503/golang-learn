75d27fb82d159d7146a6243cf7b831c2b7bb1d57
bert82503 <bert825_work@163.com>
2022-11-23 19:16:25 +0800

    add "3.For is Go's "while" for是Go中的while" of "Basics 基础" in <Go 指南>

--- tour/3.flowcontrol/for-is-gos-while.go
+++ tour/3.flowcontrol/for-is-gos-while.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-  sum := 1
-  // while
-  for sum < 1000 {
-    sum += sum
-  }
-  fmt.Println(sum)
+	sum := 1
+	// while
+	for sum < 1000 {
+		sum += sum
+	}
+	// 1024
+	fmt.Println(sum)
 }
